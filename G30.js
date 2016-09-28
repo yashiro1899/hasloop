7abdcfb8a08f23efd86f984cc12175aff1b43656
Ice White <yashiro1899@gmail.com>
2016-09-28 14:29:11 +0800

    Added two linked list,     G4.js and G30.js

---
+++ G30.js
@@ -0,0 +1,29 @@
+/**
+ * G30 Lianyungang–Khorgas Expressway
+ * The Lianyungang–Khorgas Expressway (Chinese: 连云港－霍尔果斯高速公路), commonly referred to as the Lianhuo Expressway (Chinese: 连霍高速公路), is 4,243-kilometre-long expressway (2,636 mi) in the People's Republic of China that connects the cities of Lianyungang, in the province of Jiangsu, and Khorgas, in the autonomous region of Xinjiang, on the border with Kazakhstan. At Khorgas, there is a border crossing into Kazakhstan. The expressway is the longest contiguous expressway in China with a single numeric designation, stretching across the country from the Yellow Sea on the east cost to the Kazakhstan border in the west. It passes through the provinces of Jiangsu, Anhui, Henan, Shaanxi, Gansu, and Xinjiang.
+ *
+ * From Wikipedia, the free encyclopedia
+ */
+'use strict';
+
+
+const LList = require('./lib/linked_list');
+
+var cities = new LList();
+
+cities.append("Lianyungang, Jiangsu");
+cities.append("Xuzhou, Jiangsu");
+cities.append("Kaifeng, Henan");
+cities.append("Zhengzhou, Henan");
+cities.append("Weinan, Shaanxi");
+cities.append("Xi'an, Shaanxi");
+cities.append("Baoji, Shaanxi");
+cities.append("Tianshui, Gansu");
+cities.append("Lanzhou, Gansu");
+cities.append("Wuwei, Gansu");
+cities.append("Zhangye, Gansu");
+cities.append("Jiuquan, Gansu");
+cities.append("Hami, Xinjiang");
+cities.append("Ürümqi, Xinjiang");
+
+module.exports = cities;
---
+++ G4.js
@@ -0,0 +1,38 @@
+/**
+ * G4 Beijing–Hong Kong–Macau Expressway
+ * The Beijing–Hong Kong–Macau Expressway (Chinese: 北京－香港－澳门高速公路), commonly referred to as the Jinggang'ao Expressway (Chinese: 京港澳高速公路) is a 2,272.65-kilometre-long expressway (1,412.16 mi) that connects the Chinese cities of Beijing and Shenzhen, in Guangdong province, at the border with Hong Kong. The expressway terminates at the Huanggang Port Control Point in Shenzhen, opposite the Lok Ma Chau border control point in Hong Kong. The connection to Zhuhai at the Macau border is made using the spur line G4W Guangzhou–Macau Expressway, which branches off from the main line in Guangzhou. When the expressway was completed in October 2004, it was China's first completed north-south expressway route.
+ *
+ * From Wikipedia, the free encyclopedia
+ */
+'use strict';
+
+
+const LList = require('./lib/linked_list');
+
+var cities = new LList();
+var gz;
+var mo;
+
+cities.append("Beijing");
+cities.append("Baoding, Hebei");
+cities.append("Shijiazhuang, Hebei");
+cities.append("Handan, Hebei");
+cities.append("Xinxiang, Henan");
+cities.append("Zhengzhou, Henan");
+cities.append("Luohe, Henan");
+cities.append("Xinyang, Henan");
+cities.append("Wuhan, Hubei");
+cities.append("Xianning, Hubei");
+cities.append("Yueyang, Hunan");
+cities.append("Changsha, Hunan");
+cities.append("Zhuzhou, Hunan");
+cities.append("Hengyang, Hunan");
+cities.append("Chenzhou, Hunan");
+cities.append("Shaoguan, Guangdong");
+gz = cities.append("Guangzhou, Guangdong");
+cities.append("Shenzhen, Guangdong");
+cities.append("Hong Kong");
+mo = cities.append("Macau");
+mo.next = gz;
+
+module.exports = cities;
